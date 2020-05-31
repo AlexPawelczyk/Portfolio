5c573538f9b788f0c1503178fde9c45ce8fbb7c7
AlexPawelczyk <alex.pawelczyk@uwaterloo.ca>
2020-05-31 11:06:05 -0400

    fixed bug (toggle button now works on small screens)

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-$(document).ready(function(e) {
+$(document).ready(function(e){
   $win = $(window);
-  $navbar = $("#header");
-  $toggle = $(".toggle-button");
+  $navbar = $('#header');
+  $toggle = $('.toggle-button');
   var width = $navbar.width();
   toggle_onclick($win, $navbar, width);
 
   // resize event
-  $win.resize(function() {
-    toggle_onclick($win, $navbar, width);
+  $win.resize(function(){
+      toggle_onclick($win, $navbar, width);
   });
 
-  // click on toggle (hamburger) button
-  $toggle.click(function(e) {
-    $navbar.toggleClass("toggle-left");
-  });
+  $toggle.click(function(e){
+      $navbar.toggleClass("toggle-left");
+  })
+
 });
 
-function toggle_onclick($win, $navbar, $width) {
-  if($win.width() <= 768) {
-    $navbar.css({left: "-200px"});
-  } else {
-    $navbar.css({left: "0px"});
+function toggle_onclick($win, $navbar, width){
+  if($win.width() <= 768){
+      $navbar.css({left: `-${width}px`});
+  }else{
+      $navbar.css({left: '0px'});
   }
 }
 
-var options = {
-  strings: ['Welcome to my portfolio!'],
-  typeSpeed: 40,
-  backSpeed: 50 /*,
-  loop: true */
-}
-var typed = new Typed("#typed", options);
+var typed = new Typed('#typed' , {
+  strings: [
+      'Web Developer',
+      'Freelancer'
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
 
-var options_2 = {
-  strings: ['I am a computer science enthusiast!'],
-  typeSpeed: 40,
-  backSpeed: 50 /*,
-  loop: true */
-}
-var typed_2 = new Typed("#typed_2", options_2);
+var typed_2 = new Typed('#typed_2' , {
+  strings: [
+      'Web Developer',
+      'Freelancer'
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+  anchor.addEventListener('click', function(e){
+      e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: 'smooth'
-    });
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
   });
 });
